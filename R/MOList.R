79366e66e94c522fadabe06233e87a511a1090ea
j-y26 <jielin.yang@mail.utoronto.ca>
2023-10-29 14:53:29 -0400

    input data validation

--- R/MOList.R
+++ R/MOList.R
@@ -45,6 +45,77 @@ setClass("MOList",
   )
 )
 
+# Validate matrix and grouping information
+# Level: Private
+# @param matrix A numeric matrix containing the sequencing data
+# @param groupBy A vector of grouping information for the sequencing data
+validateMatrix <- function(matrix, groupBy) {
+  if (any(is.na(matrix))) {
+    stop("The sequencing data contains NA values. Please check the validity of
+    your data.")
+  } else if (is.na(groupBy)) {
+    stop("Please provide grouping information for the sequencing data.")
+  } else if (!is.numeric(matrix)) {
+    stop("The sequencing data must be a numeric matrix.")
+  } else if (ncol(matrix) != length(groupBy)) {
+    stop("The number of samples in the sequencing data must match the length of
+    the grouping information.")
+  } else {
+    # Do nothing
+  }
+}
+
+# Validate the inputs for the MOList constructor
+# Level: Private
+# @param RNAseq A numeric matrix containing the RNAseq data
+# @param RNAGroupBy A vector of grouping information for the RNAseq data
+# @param smallRNAseq A numeric matrix containing the smallRNAseq data
+# @param smallRNAGroupBy A vector of grouping information for the smallRNAseq
+#                        data
+# @param proteomics A numeric matrix containing the proteomics data
+# @param proteomicsGroupBy A vector of grouping information for the proteomics
+#                          data
+# @param peakCond1 A data frame containing the ATAC peaks for condition 1
+# @param peakCond2 A data frame containing the ATAC peaks for condition 2
+validateMOInputs <- function(RNAseq,
+                             RNAGroupBy,
+                             smallRNAseq,
+                             smallRNAGroupBy,
+                             proteomics,
+                             proteomicsGroupBy,
+                             peakCond1,
+                             peakCond2) {
+  # Validate the RNAseq data
+  validateMatrix(RNAseq, RNAGroupBy)
+  # Validate the smallRNAseq data
+  if (!is.na(smallRNAseq)) {
+    validateMatrix(smallRNAseq, smallRNAGroupBy)
+  } else {
+    # Do nothing
+  }
+  # Validate the proteomics data
+  if (!is.na(proteomics)) {
+    validateMatrix(proteomics, proteomicsGroupBy)
+  } else {
+    # Do nothing
+  }
+  # Replicates are required for the sequencing data
+  if (ncol(RNAseq) < 2 || ncol(smallRNAseq) < 2 || ncol(proteomics) < 2) {
+    stop("At least two replicates are required for each sequencing data.")
+  } else {
+    # Do nothing
+  }
+  # Check for missing values in the chromosome information in the ATAC peaks
+  chromInfo <- c("chrom", "chromStart", "chromEnd")
+  if (any(is.na(peakCond1[, chromInfo])) || 
+      any(is.na(peakCond2[, chromInfo]))) {
+    stop("Missing values in the chromosome information in the ATAC peaks.")
+  } else {
+    # Do nothing
+  }
+}
+
+
 
 
 
